d63fa015371e0c10f998cf4bc324952037aad8a0
Claudionor Amâncio <claudionorojr@hotmail.com>
2020-10-16 15:50:38 -0300

    Adicionados comentários ao código da cifra monoalfabetica

--- monoalfabetica.py
+++ monoalfabetica.py
@@ -1,13 +1,21 @@
+# Discentes: João Roberto & Claudionor Amâncio
+# Importação de arquivo caracteres.py, que foi criado e está na pasta lib;
+# e importação da biblioteca sys
 from lib.caracteres import *
 import sys
 
+#Declaração de variáveis. ALFB é uma tabela que aceita os caracteres em minúsculo
+#transformados pela função lower_case().
 ALFB = lower_case()
 BFLA = []
 temp = simbols()
 
+#Preenche a tabela BFLA com a função simbols que recebe os simbolos da tabela ALFB
 for i in range(0, 28):
     BFLA.append(temp[i])
 
+#Busca o indice do caractere da mensagem na tabela ALBF e altera pelo caractere
+#que esta na mesma posição na tabela BLFA
 def encript(msg, ALFB, BFLA):
     code = ''
     for i in msg:
@@ -16,6 +24,7 @@ def encript(msg, ALFB, BFLA):
         code+=x
     return code
 
+#Realiza a operação contrária da função de encriptação.
 def decript(code, ALFB, BFLA):
     msg = ''
     for i in code:
@@ -24,6 +33,7 @@ def decript(code, ALFB, BFLA):
         msg+=x
     return msg
 
+# Função para abrir o arquivo .txt
 def openFile(argv):
     try:
         arq = open(argv, 'r')
@@ -33,12 +43,14 @@ def openFile(argv):
     except:
         return 1
 
+# Função para alteração do arquivo .txt
 def changeFile(argv, msg):
         arq = open(argv, 'w')
         arq.write(msg)
         arq.close()
 
-
+# Declaração dos argumentos por linha da comando para:
+# Encriptação
 if sys.argv[1] == '-e':
     msg = openFile(sys.argv[2])
     if(msg != 1):
@@ -46,6 +58,7 @@ if sys.argv[1] == '-e':
         changeFile(sys.argv[2], msg)
     else:
         print("ERRO: Arquivo nao existente")
+# Decriptação
 elif sys.argv[1] == '-d':
     msg = openFile(sys.argv[2])
     if(msg != 1):
@@ -53,12 +66,14 @@ elif sys.argv[1] == '-d':
         changeFile(sys.argv[2], msg)
     else:
         print("ERRO: Arquivo nao existente")
+# Modo de interação
 elif sys.argv[1] == '-a':
     msg = input("Encript >>> ")
     msg = encript(msg, ALFB, BFLA)
     print(msg)
     msg = decript(msg, ALFB, BFLA)
     print(msg)
+# Comando para ajuda
 elif sys.argv[1] == '-h':
     print("CIFRA MONOALFABETICA\nComo usar:")
     print("-e para criptografar um arquivo .txt\npython monoalfabeica.py -e <arquivo.txt>\n")
